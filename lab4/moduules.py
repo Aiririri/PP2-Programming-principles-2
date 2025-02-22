d2e537e55b48563e6d5666e75c7d0b214aba8f0c
Aiririri <amirbaiguzhinov2005@gmail.com>
2025-02-22 09:06:34 +0500

    Add files via upload

---
+++ lab4/moduules.py
@@ -0,0 +1,24 @@
+import mymodule
+
+mymodule.greeting("Jonathan")
+
+
+
+a = mymodule.person1["age"]
+print(a)
+"""import mymodule as mx
+
+a = mx.person1["age"]
+print(a)
+"""
+
+
+import platform
+
+x = platform.system()
+print(x)
+
+
+
+x = dir(platform)
+print(x)
